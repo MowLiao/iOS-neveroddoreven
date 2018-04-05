1c21ae4a0b1850c44c805d849930a2aa5d0810d5
Martin Tsenov [sc14mtt] <sc14mtt@csmac001.leeds.ac.uk>
2018-04-05 17:29:42 +0100

    Cleanup and Marker out of view WIP

--- NotYourBusiness/ViewController.swift
+++ NotYourBusiness/ViewController.swift
@@ -9,18 +9,26 @@
 import UIKit
 import GoogleMaps
 
-var locationManager = CLLocationManager()
 
 class ViewController: UIViewController,GMSMapViewDelegate,CLLocationManagerDelegate {
-    
     @IBOutlet var mapView: GMSMapView!
     
     var locationManager = CLLocationManager()
     var camera = GMSCameraPosition()
+    var marker = GMSMarker()
     
+
+    override func loadView() {
+        //let camera = GMSCameraPosition.camera(withLatitude: 53.805114, longitude: -1.555301, zoom: 18.7)
+        mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+        mapView.settings.setAllGesturesEnabled(true)
+        
+        view = mapView
+        print("loadView")
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         
         mapView.isMyLocationEnabled = true
         mapView.delegate = self
@@ -28,37 +36,9 @@ class ViewController: UIViewController,GMSMapViewDelegate,CLLocationManagerDeleg
         //Location Manager code to fetch current location
         self.locationManager.delegate = self
         self.locationManager.startUpdatingLocation()
-
-//        mapView.isMyLocationEnabled = true
-//        mapView.delegate = self
-
+        print("didload")
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    override func loadView() {
-//        let camera = GMSCameraPosition.camera(withLatitude: 53.805114, longitude: -1.555301, zoom: 18.7)
-        mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
-        
-        view = mapView
-        mapView.animate(to: camera)
-
-//        // Create a GMSCameraPosition that tells the map to display the
-//        // coordinate -33.86,151.20 at zoom level 6.
-//
-//        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
-//
-//        // Creates a marker in the center of the map.
-//        let marker = GMSMarker()
-//        marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
-//        marker.title = "Sydney"
-//        marker.snippet = "Australia"
-//        marker.map = mapView
-    }
-
+    
     //Location Manager delegates
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         
@@ -66,37 +46,52 @@ class ViewController: UIViewController,GMSMapViewDelegate,CLLocationManagerDeleg
         
         camera = GMSCameraPosition.camera(withLatitude: (location?.coordinate.latitude)!, longitude:(location?.coordinate.longitude)!, zoom:18.7)
         mapView.animate(to: camera)
-//        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+//        print(isMarkerWithinScreen(marker: location!))
+        
         //Finally stop updating location otherwise it will come again and again in this delegate
         self.locationManager.stopUpdatingLocation()
         
     }
     
-    @IBAction func changeMapType(sender: AnyObject) {
-        let actionSheet = UIAlertController(title: "Map Types", message: "Select map type:", preferredStyle: UIAlertControllerStyle.actionSheet)
-        
-        let normalMapTypeAction = UIAlertAction(title: "Normal", style: UIAlertActionStyle.default) { (alertAction) -> Void in
-            self.mapView.mapType = GMSMapViewType.normal
-        }
-        
-        let terrainMapTypeAction = UIAlertAction(title: "Terrain", style: UIAlertActionStyle.default) { (alertAction) -> Void in
-            self.mapView.mapType = GMSMapViewType.terrain
-        }
-        
-        let hybridMapTypeAction = UIAlertAction(title: "Hybrid", style: UIAlertActionStyle.default) { (alertAction) -> Void in
-            self.mapView.mapType = GMSMapViewType.hybrid
-        }
-        
-        let cancelAction = UIAlertAction(title: "Close", style: UIAlertActionStyle.cancel) { (alertAction) -> Void in
-            
-        }
-        
-        actionSheet.addAction(normalMapTypeAction)
-        actionSheet.addAction(terrainMapTypeAction)
-        actionSheet.addAction(hybridMapTypeAction)
-        actionSheet.addAction(cancelAction)
-        
-        present(actionSheet, animated: true, completion: nil)
+    // WIP
+    func isMarkerWithinScreen(marker: CLLocation) -> Bool {
+        let region = self.mapView.projection.point(for: marker.coordinate)
+//        let bounds = GMSCoordinateBounds(region: region)
+        print(region)
+        return true
     }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+//    @IBAction func changeMapType(sender: AnyObject) {
+//        let actionSheet = UIAlertController(title: "Map Types", message: "Select map type:", preferredStyle: UIAlertControllerStyle.actionSheet)
+//        
+//        let normalMapTypeAction = UIAlertAction(title: "Normal", style: UIAlertActionStyle.default) { (alertAction) -> Void in
+//            self.mapView.mapType = GMSMapViewType.normal
+//        }
+//        
+//        let terrainMapTypeAction = UIAlertAction(title: "Terrain", style: UIAlertActionStyle.default) { (alertAction) -> Void in
+//            self.mapView.mapType = GMSMapViewType.terrain
+//        }
+//        
+//        let hybridMapTypeAction = UIAlertAction(title: "Hybrid", style: UIAlertActionStyle.default) { (alertAction) -> Void in
+//            self.mapView.mapType = GMSMapViewType.hybrid
+//        }
+//        
+//        let cancelAction = UIAlertAction(title: "Close", style: UIAlertActionStyle.cancel) { (alertAction) -> Void in
+//            
+//        }
+//        
+//        actionSheet.addAction(normalMapTypeAction)
+//        actionSheet.addAction(terrainMapTypeAction)
+//        actionSheet.addAction(hybridMapTypeAction)
+//        actionSheet.addAction(cancelAction)
+//        
+//        present(actionSheet, animated: true, completion: nil)
+//    }
 }
 
